2d31357453ba8513fb4249b730701730dfe79e1a
Minjae Choi <chlalswo200@naver.com>
2022-11-16 21:20:43 +0900

    22.11.16 algorithm

---
+++ 01_Bronze/5/16099.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+for _ in range(int(input())):
+    lt, wt, le, we = map(int, input().split())
+
+    if lt * wt > le * we:
+        print('TelecomParisTech')
+    elif lt * wt < le * we:
+        print('Eurecom')
+    else:
+        print('Tie')
